5e1b20c13a97dfa3ab65093281da33b06c7cca51
Вадим <118062456+DeveloperVadim@users.noreply.github.com>
2023-02-11 19:44:19 +0700

    fix

--- les_3/task_3.py
+++ les_3/task_3.py
@@ -42,3 +42,18 @@ while True:
         features[f] = int(feature_) if (f == 'цена' or f == 'количество') else feature_
         analytics[f].append(features[f])
     goods.append((num, features))
+
+# goods = int(input("Введите количество товара "))
+# n = 1
+# my_dict = []
+# my_list = []
+# my_analys = {}
+# while n <= goods:
+#     my_dict = dict({'название': input("введите название "), 'цена': input("Введите цену "),
+#                     'количество': input('Введите количество '), 'eд': input("Введите единицу измерения ")})
+#     my_list.append((n, my_dict))
+#     n += 1
+#     my_analys = dict({'название': my_dict.get('название'), 'цена': my_dict.get('цена'), 'количество': my_dict.get('количество'),
+#          'ед': my_dict.get('ед')})
+# print(my_list)
+# print(my_analys)
